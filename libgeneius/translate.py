4f36e9afa703453edb575061048cf03c0bffe35e
ajish <ajishg@gmail.com>
2010-06-18 16:15:26 -0400

    bugfix

--- libgeneius/translate.py
+++ libgeneius/translate.py
@@ -66,7 +66,7 @@ def getMutationEffects(genomes_rule, map, pos, newbases):
             effect = ["deletion", "frameshift"]
         elif len(newbase) > 1 and (len(newbase)-1) % 3 != 0:
             effect = ["insertion", "frameshift"]
-        elif (len(newbase)-1) % 3 == 0:
+        elif len(newbase) > 1 and (len(newbase)-1) % 3 == 0:
             effect = ["insertion", "inframe"]
         elif newbase == original["allele"]:
             effect = ["reference"]
